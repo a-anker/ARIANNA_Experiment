b7a45190c577aa38e74cbb5952cf9f1216820337
a-anker <38436394+a-anker@users.noreply.github.com>
2023-05-03 16:51:45 -0700

    Update data_trace_trimmer.py

--- data_trace_trimmer.py
+++ data_trace_trimmer.py
@@ -5,27 +5,22 @@ import os
 import operator
 from scipy.signal import find_peaks
 
+######################################################################################################################
+"""
+This takes in 1 channels worth of data with size (event_num, num_of_samps), trims it to a specified window around the max value, and then saves it as a numpy array
+If for example the window size is 100 and the maximum occurs beflore 50 or after 206, the first 100 sampoles or last 100 samples is chosen instead.
+"""
+######################################################################################################################
+PathToARIANNA = os.environ['ARIANNA_Experiment']
+input_File = np.load(PathToARIANNA + '/data/arianna_data.py')
 
-data = np.load('/Users/astrid/Desktop/ML_paper/cross_correlation_study/data_signal_LPDA_ch0_0.036sa_10.0vrms_no_noise.npy')
-
-input_file = data
-window_size = 100
-t_len = 256  # trace length
+window_size = 100 # final trace length
+t_len = 256  # input trace length
 
 times = np.arange(t_len)
 trimmed_array = np.empty((len(input_file), window_size))
 for i, event in enumerate(input_file):
-    # event = event[0]
-    # diffs1 = np.zeros(len(event) - 2)
-    # diffs2 = np.zeros(len(event) - 2)
-    # for j in range(len(event) - 2):
-    #     diffs1[j] = abs(event[j + 1] - event[j])
-    #     diffs2[j] = abs(event[j + 2] - event[j])
-
-    # index1, mid_pt1 = max(enumerate(diffs1), key=operator.itemgetter(1))
-    # index2, mid_pt2 = max(enumerate(diffs2), key=operator.itemgetter(1))
-    # if mid_pt1 > mid_pt2:
-    #     mid_pt = index1
+    
     index1, mid_pt1 = max(enumerate(event), key=operator.itemgetter(1))
     index2, mid_pt2 = min(enumerate(event), key=operator.itemgetter(1))
     if abs(mid_pt1) > abs(mid_pt2):
@@ -35,17 +30,10 @@ for i, event in enumerate(input_file):
 
     if mid_pt > window_size / 2 - 1 and mid_pt < (t_len - 1) - window_size / 2:
         trimmed_array[i] = event[int(mid_pt - window_size / 2):int(mid_pt + window_size / 2)]
-        # plt.plot(times[int(mid_pt - window_size / 2):int(mid_pt + window_size / 2)], trimmed_array[i])
     elif mid_pt > (t_len - 1) - window_size / 2:
         trimmed_array[i] = event[(t_len - 1) - window_size:(t_len - 1)]
-        # plt.plot(times[255 - window_size:255], trimmed_array[i])
     elif mid_pt < window_size / 2:
         trimmed_array[i] = event[0:int(window_size)]
         mid_pt = int(window_size / 2)
-        # plt.plot(times[int(mid_pt - window_size / 2):int(mid_pt + window_size / 2)], trimmed_array[i])
-
-        # plt.plot(times[int(mid_pt - window_size / 2):int(mid_pt + window_size / 2)], trimmed_array[i])
-        # plt.plot(np.linspace(1, 240, 240), event)
-        # plt.show()
-
-save(os.path.join("/Users/astrid/Desktop/ML_paper/cross_correlation_study/trimmed100_data_signal_LPDA_ch0_0.036sa_10.0vrms_no_noise.npy"), trimmed_array)  # change
+     
+save(os.path.join(PathToARIANNA, '/data/trimmed100_arianna_data.py')), trimmed_array)
