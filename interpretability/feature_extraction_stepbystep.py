637aae6455ead225f0422beebe33ea8fd395b591
a-anker <38436394+a-anker@users.noreply.github.com>
2023-05-31 13:26:13 -0700

    Update feature_extraction_stepbystep.py

--- interpretability/feature_extraction_stepbystep.py
+++ interpretability/feature_extraction_stepbystep.py
@@ -45,6 +45,129 @@ def plot_input_wf():
 
 	# plt.show()
 
+	
+def main(evt_num,chs,label):
+
+	data = noise
+
+	manual_data = data[evt_num][0:chs,:,0] # (8,256,1)
+	
+	pred_data = data
+	model = keras.models.load_model(f'/Users/astrid/Desktop/st61_deeplearning/h5_models/trained_CNN_1l-10-8-10-do0.5_fltn_sigm_valloss_p4_allampl37-74k_5-10ksig_shuff_monitortraining_1.h5')
+	# model = keras.models.load_model(f'/Users/astrid/Desktop/st61_deeplearning/h5_models/trained_CNN_1l-10-2-10-do0.5_fltn_sigm_valloss_p4_allampl37-74k_5-10ksig_shuff_monitortraining_2,3dlpda_4.h5')
+	# model = keras.models.load_model(f'/Users/astrid/Desktop/st61_deeplearning/h5_models/trained_CNN_1l-10-8-20_do0.5_fltn_sigm_50k_simnoise_0-5k_simsig_0.h5')
+
+	# model.summary()
+	
+	print(model.predict(pred_data[:,0:chs])[evt_num])
+	
+
+	#______________________________________
+	#plot input waveform
+	x = np.linspace(1, 256, 256)
+	fig, axs = plt.subplots(chs, sharex=True)
+	plt.xlim(1,256)
+	for i in range(chs):
+		axs[i].plot(x, data[evt_num][i,:,0])
+		# axs[i].set(ylim=(-0.1,0.1))
+	axs[chs-1].set(xlabel='sample')
+	axs[0].set(ylabel='V')
+	axs[0].set(title=f'{label} {evt_num}, classif={model.predict(pred_data[:,0:chs])[evt_num]}')
+	plt.show()
+	#______________________________________
+
+	weights = model.get_weights()[0]  # (8, 10, 1, 10)
+	weights = weights[:,:,0,:]  # shape is (fil size1,fil size2, n_filters)
+	filter_size1,filter_size2, n_filters = weights.shape
+	w_bias = model.get_weights()[1]  # (10,)
+	fc = model.get_weights()[2]  # (2470x1)
+	fc_bias = model.get_weights()[3]  # (1,)
+
+	
+
+	shifts = 256 - filter_size2 + 1
+	second_dim = int(manual_data.shape[1] / filter_size2) - 1
+
+	# convolution
+	s1 = np.zeros((shifts, n_filters))
+	for i in range(n_filters):
+	    weight = weights[:, :,i]
+
+	    for j in range(shifts):
+	        
+	        s1[j][i] = np.sum(manual_data[:,j:j + filter_size2] * weight) + w_bias[i]
+	#______________________________________
+	#plot convolutions
+	x = np.linspace(5, 15, 10)
+	fig, axs = plt.subplots(chs, sharex=True)
+	plt.xlim(1,256)
+	for i in range(chs):
+		axs[i].plot(x, weights[i,:,0])
+		axs[i].plot(x+25, weights[i,:,1])
+		axs[i].plot(x+50, weights[i,:,2])
+		axs[i].plot(x+75, weights[i,:,3])
+		axs[i].plot(x+100, weights[i,:,4])
+		axs[i].plot(x+125, weights[i,:,5])
+		axs[i].plot(x+150, weights[i,:,6])
+		axs[i].plot(x+175, weights[i,:,7])
+		axs[i].plot(x+200, weights[i,:,8])
+		axs[i].plot(x+225, weights[i,:,9])
+		# axs[i].set(ylim=(-2.75,2.75))
+
+	axs[chs-1].set(xlabel='sample')
+	axs[0].set(ylabel='V')
+	axs[0].set(title=f'{label} {evt_num} convolution weights 10 {chs}x10 filter')
+	plt.show()
+	#______________________________________
+
+
+	#______________________________________
+	# plot the 10 convolution filter waveforms after convoluting the 10 8x10 filters and adding bias
+	s1[s1 <= 0] = 0 #uncomment to plot the same thing below with relu activation applied
+	x3 = np.linspace(1, shifts, shifts)
+	fig3, axs3 = plt.subplots(10, sharex=True)
+	for i in range(10):
+		axs3[i].plot(x3, s1[:,i])
+		# axs3[i].set(ylim=(-1,25))
+
+	axs3[0].set(title=f'{label} {evt_num}: relu activation applied ')
+	# axs3[0].set(title='noise: result of convolution weights, 10 filters ')
+	plt.show()
+	#______________________________________
+
+	# relu activation
+	s1[s1 <= 0] = 0
+
+
+	# # max pooling
+	# s2 = np.zeros((second_dim, n_filters))
+	# for i in range(n_filters):
+	#     for k in range(second_dim):
+	#         j = 10 * k
+	#         s2[k][i] = max(s1[j:j + 10, i])  # s2 is 23x10
+
+	# flatten and fc layer
+	# s2 = s2.T
+
+	s3 = s1.flatten()
+
+	s4 = np.matmul(fc[:, 0], s3) + fc_bias[0]
+	print(s4)
+
+	# #______________________________________
+	#plots the step with flattening applied and also the fc weights plot together
+	plt.plot(np.linspace(1,s3.shape[0],s3.shape[0]),fc[:, 0],color='red',label='fc weights')
+	plt.plot(np.linspace(1,s3.shape[0],s3.shape[0]),s3,color='blue',label='flattening applied')
+	plt.title(f'{label} {evt_num}: these multiplied together, ~{round(fc_bias[0])} bias added result: {round(s4,3)}')
+	# plt.title(f'noise: flattening applied')
+	# plt.ylim(-.5,5.5)
+	# plt.legend()
+	plt.show()
+	# #______________________________________	
+	
+	
+	
+	
 
 def dl_stepbystep(data, evt_num, label):
 	manual_data = data[evt_num] # (2048,1,1)
